353a38e8db8b3b0d8e515c759e329c939f88de7f
Avisa Poshtkouhi <aposhtkouhi@gmail.com>
2020-07-16 15:20:54 -0700

    Initial commit

---
+++ Courrency-App/Courrency-App/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Courrency-App
+//
+//  Created by Avisa Poshtkouhi on 7/16/20.
+//  Copyright © 2020 Avisa Poshtkouhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Courrency-App/Courrency-App/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Courrency-App
+//
+//  Created by Avisa Poshtkouhi on 7/16/20.
+//  Copyright © 2020 Avisa Poshtkouhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
